bb25542a5bd200b30ab78fa4a7e91e2051b0e8c7
Giovani Candido <giovcandido@outlook.com>
2022-09-16 12:16:45 +0200

    Create recipes app

---
+++ recipes/views.py
@@ -0,0 +1,15 @@
+from django.http import HttpResponse
+
+# from django.shortcuts import render
+
+
+def home(request):
+    return HttpResponse('Home')
+
+
+def about(request):
+    return HttpResponse('About')
+
+
+def contact(request):
+    return HttpResponse('Contact')
