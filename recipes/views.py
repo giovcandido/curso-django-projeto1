061d73c5786225e42a7e5686dd114c71e2d03ebd
Giovani Candido <giovcandido@outlook.com>
2022-10-12 12:09:53 -0300

    Create the base website

--- recipes/views.py
+++ recipes/views.py
@@ -2,6 +2,4 @@ from django.shortcuts import render
 
 
 def home(request):
-    return render(request, 'recipes/home.html', context={
-        'name': 'Giovani Candido',
-    })
+    return render(request, 'recipes/pages/home.html')
