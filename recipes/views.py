87649f3934ce80e132556ba033637a8181a1e287
Giovani Candido <giovcandido@outlook.com>
2022-10-11 21:47:31 -0300

    Remove contact and about, and create templates

--- recipes/views.py
+++ recipes/views.py
@@ -1,15 +1,7 @@
-from django.http import HttpResponse
-
-# from django.shortcuts import render
+from django.shortcuts import render
 
 
 def home(request):
-    return HttpResponse('Home')
-
-
-def about(request):
-    return HttpResponse('About')
-
-
-def contact(request):
-    return HttpResponse('Contact')
+    return render(request, 'recipes/home.html', context={
+        'name': 'Giovani Candido',
+    })
